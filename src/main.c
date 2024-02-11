b9a04f2010ad15e3e1a0e0477afcc4ee83ab0506
Noa Roussiere <noa.roussiere@epitech.eu>
2024-02-11 01:46:33 +0100

    feat: :sparkles: Addings env program

--- src/main.c
+++ src/main.c
@@ -8,13 +8,13 @@
 #include "minishell1.h"
 #include "library.h"
 
-int main(int ac, char **av)
+int main(int ac, char **av, char **env)
 {
     if (ac == 1 && my_strcmp(av[0], "./mysh") == 0) {
         if (isatty(STDIN_FILENO)) {
-            return get_input_user();
+            return get_input_user(env);
         } else
-            return non_interactive();
+            return non_interactive(env);
     } else
         return 84;
     return 0;

dbcf7bb4cc86308b60e170edc8b666c197ee6681
Noa Roussiere <noa.roussiere@epitech.eu>
2024-02-11 02:20:32 +0100

    feat: :sparkles: the cd function work if the user put cd and the path but not if he put only cd or cd ~ or cd -

--- includes/minishell1.h
+++ includes/minishell1.h
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <dirent.h>
 #include <fcntl.h>
 
 #ifndef MINISHELL1_H_
--- src/cd/main_cd.c
+++ src/cd/main_cd.c
@@ -8,12 +8,28 @@
 #include "minishell1.h"
 #include "library.h"
 
+static int go_folder (char *src)
+{
+    if (opendir(src) == NULL)
+        return 84;
+    else
+        chdir(src);
+    return 0;
+}
+
 int main_cd(int ac, char **av, char **env)
 {
     if (env != NULL)
         env = NULL;
     if (ac == 1 && my_strcmp(av[0], "cd") == 0) {
         chdir(getenv("HOME"));
+        return 0;
+    }
+    if (ac == 2 && my_strcmp(av[0], "cd") == 0) {
+        if (my_strcmp(av[1], "~") != 0 && my_strcmp(av[1], "-") != 0) {
+            return go_folder(av[1]);
+        } else
+            return 84;
     } else
         return 84;
     return 0;
